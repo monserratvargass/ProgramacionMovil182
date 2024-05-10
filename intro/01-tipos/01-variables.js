a7eccdfef380fdbb87ffc413b642fdb1ebe97dcc
monserratvargass <guvamonse@gmail.com>
2024-05-09 23:00:05 -0600

    reglas para nombrar variables

--- intro/01-tipos/01-variables.js
+++ intro/01-tipos/01-variables.js
@@ -3,3 +3,19 @@ let nombre = "Hola mundo";
 console.log(nombre);
 
 /* UNDIFINED: variable que se ha declarado pero que no se le asigna ningun valor */
+
+/* REGLAS: 
+Inicia siempre con letras
+No pueden ser nombradas con palabras reservadas */
+
+/* CONVENCIONES */
+UpperCamelCase
+let NombreCompleto;
+camelCase 
+let nombreCompleto;
+snake_case
+let nombre_completo;
+
+/* Asignacion de valores y se pueden definir muchas variables */
+let apellido, animal;
+apellido = 'Schurmann';
