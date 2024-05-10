f3b39cb736d0f36ba679b883c5b20e79d6c4788a
monserratvargass <guvamonse@gmail.com>
2024-05-09 23:24:53 -0600

    tipado dinamico

---
+++ intro/01-tipos/04-tipado-dinamico.js
@@ -0,0 +1,14 @@
+let numero = 42;
+let nombre = "hola mundo";
+let verdadero = true;
+let undef;
+let nula = null;
+
+/* typeof: indica el tipo de variable */
+console.log(typeof numero);
+console.log(typeof nombre);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nula);
+
+
