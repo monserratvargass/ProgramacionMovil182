8866328d8793f0760ccb6bfd0cc2dfc8aa9424eb
monserratvargass <guvamonse@gmail.com>
2024-05-10 14:31:51 -0600

    comparación

---
+++ intro/02-operadores/03-comparacion.js
@@ -0,0 +1,10 @@
+let a = 10;
+console.log(a > 5)
+console.log(a >= 5)
+console.log(a < 5)
+console.log(a <= 5)
+
+console.log(a == 10) //Solo por el valor
+console.log(a != 10)
+console.log(a === '10') //Valor y tipo de dato
+console.log(a !== 5)
