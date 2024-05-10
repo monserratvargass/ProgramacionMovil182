d2b402887806bfa81398c2aad6f7d16ca038f1f8
monserratvargass <guvamonse@gmail.com>
2024-05-10 00:25:25 -0600

    operadores aritméticos

---
+++ intro/02-operadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+/* console.log(a + b, 'Adicion'); //Adicion
+console.log(a - b, 'Resta'); //Resta
+console.log(a * b, 'Multiplicacion'); //Multiplicacion
+console.log(a / b, 'Division'); //Division
+console.log(a % b, 'Modulo'); //Modulo
+console.log(a ** b, 'Potencia'); //Potencia */
+
+//Incrementar
+console.log(++a)
+console.log(a++)
+console.log(a)
+//Decrementar
+console.log(--a)
+console.log(a--)
+console.log(a)
