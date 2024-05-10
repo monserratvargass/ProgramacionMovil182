ecfab09a777762d31fce8b44c040e49170b3f040
monserratvargass <guvamonse@gmail.com>
2024-05-09 23:10:03 -0600

    tipos de datos primitivos

---
+++ intro/01-tipos/02-primitivos.js
@@ -0,0 +1,7 @@
+let numero = 1;
+let texto = "hola mundo";
+let verdadero = true;
+let falso = false;
+let noDefinido; /* undefined */
+let undef = undefined; /* Tipo de dato o valor */
+let nulo = null; /* Esto tiene que tener un valo, pero su valor es nulo */
