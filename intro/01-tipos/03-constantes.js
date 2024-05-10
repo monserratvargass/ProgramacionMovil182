290a515610f4cdd98b731edfe1aaf23432e05529
monserratvargass <guvamonse@gmail.com>
2024-05-09 23:15:39 -0600

    constantes

---
+++ intro/01-tipos/03-constantes.js
@@ -0,0 +1,6 @@
+/* Cuando declaramos variables como constantes no vamos a poder cambiar su valor de esa variable */
+
+const nombre = "Hola mundo";
+/* nombre = "Chanchito feliz"; */
+
+console.log(nombre);
