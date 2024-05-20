2fbaff428071cef05e39c2fd74fe7bddc97438f4
monserratvargass <guvamonse@gmail.com>
2024-05-20 15:42:00 -0600

    avance practica2

---
+++ JS4React/index.js
@@ -0,0 +1,30 @@
+/* function suma(a,b){
+    return a+b;
+} */
+
+/* //Function fatArrow
+const suma=(a,b)=>{
+    return a+b;
+}; */
+
+/* //Version simplificada
+const suma=(a,b)=>a+b; 
+
+console.log(suma(2,3)); */
+
+//OBJETOS
+
+const usuario={
+    nombre:'Monse',
+    apellidoP: 'Gudiño',
+    edad: 22,
+    direccion:{
+        pais:'Mexico',
+        ciudad:'Querétaro',
+        calle:'Calandrias'
+    },
+    //ARREGLOS
+    amigos:['Lilian','Alan'],
+    estatus: true,
+    enviarCorreo: 
+}
