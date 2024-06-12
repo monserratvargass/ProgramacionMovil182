66d3eff6fa322f5ad6c85a1e4d1c9e2d3f1c774f
monserratvargass <guvamonse@gmail.com>
2024-06-12 15:27:54 -0600

    Componentes, Props y State (texto y boton)

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,15 +1,42 @@
+//Importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react'; //Importacion para hacer usos de estado
 
+const Texto=()=>{ /* Parametros */
+  const[contenido, setContenido] = useState('holaMundo') /* Variable de contenido (propiedad children), se u */
+  const actualizarContenido=()=>{/* Array function que va a permitir hacer esa actualizacion */
+    setContenido('State actualizó este texto')
+  } 
+  return(<Text onPress={actualizarContenido}>{contenido}</Text>) /* Mandar llamar el evento */
+}
+
+const Boton=()=>{ /* Parametros */
+  const[contenido, setContenido] = useState('Presioname') /* Variable de contenido (propiedad children), se u */
+  const actualizarContenido=()=>{/* Array function que va a permitir hacer esa actualizacion */
+    setContenido('State actualizó este boton')
+  } 
+  return(<Button title={contenido} color="#000000" onPress={actualizarContenido}>{contenido}</Button>) /* Mandar llamar el evento */
+}
+
+//Existen componentes, area donde se va a ejecutar
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      {/* Dinamico la asignacion del texto */}
+      <Texto contenido={'Hola mundo'}></Texto> {/* Anidar otros componente dentro de otro componente */}
+      <Texto contenido={'ReactNative'}/>
+
+      <Boton></Boton>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//Configuraciones de hojas de estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
