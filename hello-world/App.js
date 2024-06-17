73a458c34be9dc34a247e32bbb441aacb7afc1ce
monserratvargass <guvamonse@gmail.com>
2024-06-17 17:31:12 -0600

    TextInput & Button

--- hello-world/App.js
+++ hello-world/App.js
@@ -1,41 +1,25 @@
 //Importaciones
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import React,{useState} from 'react'; //Importacion para hacer usos de estado
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
+import React,{useState} from 'react'; //Importacion para hacer usos de estado de los componentes
 
-//se crea una propiedad estilo unica para el componente texto
-const Texto=({estilo})=>{ /* Parametros */
-  const[contenido, setContenido] = useState('holaMundo') /* Variable de contenido (propiedad children), se u */
-  const actualizarContenido=()=>{/* Array function que va a permitir hacer esa actualizacion */
-    setContenido('State actualizó este texto')
-  } 
-  return(<Text style={[styles.text,estilo]} onPress={actualizarContenido}>{contenido}</Text>) /* Mandar llamar el evento */
-}
-
-const Boton=()=>{ /* Parametros */
-  const[contenido, setContenido] = useState('Presioname') /* Variable de contenido (propiedad children), se u */
-  const actualizarContenido=()=>{/* Array function que va a permitir hacer esa actualizacion */
-    setContenido('State actualizó este boton')
-  } 
-  return(<Button title={contenido} color="#000000" onPress={actualizarContenido}>{contenido}</Button>) /* Mandar llamar el evento */
-}
 
 //Existen componentes, area donde se va a ejecutar
 export default function App() {
+
+  const[text, setText]=useState('Valor default')
+
+  const[submit, setSubmit]=useState('')
+
+
   return (
 
     <View style={styles.container}>
 
-      {/* Dinamico la asignacion del texto */}
-      {/* <Texto contenido={'Hola mundo'}></Texto> */} {/* Anidar otros componente dentro de otro componente */}
-{/*       <Texto contenido={'ReactNative'}/>
+      <Text> Componente TextInput:{submit} </Text>
+      <TextInput style={styles.input} placeholder='Ingresa tu nombre' onChangeText={(t)=>setText(t)} value={text}/>
 
-      <Boton></Boton> */}
-      
-      {/* Mandar llamar a la propiedad estilo de nuestra hoja de estilos con nuestras clases */}
-      <Texto estilo={styles.red}/>
-      <Texto estilo={styles.green}/>
-      <Texto estilo={styles.blue}/>
+      <Button title='Presioname' onPress={()=>{ setSubmit(text); alert('Texto enviado') } }/> {/* Funcion anonima */}
       
       <StatusBar style="auto" />
     </View>
@@ -49,32 +33,16 @@ const styles = StyleSheet.create({
     flexDirection:'column-reverse',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
   },
 
-  text:{
-    color:'yellow',
-    fontSize: 25,
-    //Asignar un alto y ancho
-/*     height:150,
-    width:150, */
+  input: {
+    width: '80%',
+    height: 40,
+    margin: 12,
+    borderBottomWidth: 1,
+    padding: 10,
+    borderBottomColor: 'blue',
   },
-  
-  //Asignacion de colores CLASES
-  red:{
-    /* flex:1, */
-    backgroundColor: 'red',
-  },
-
-  green:{
-    /* flex:2, */
-    backgroundColor: 'green',
-  },
-
-  blue:{
-    /* flex:3, */
-    backgroundColor: 'blue',
-  },
-
 
 });
