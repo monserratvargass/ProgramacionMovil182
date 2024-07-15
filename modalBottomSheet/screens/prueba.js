81946e2c74a9a8e652fd96bc16a483051682ac90
monserratvargass <guvamonse@gmail.com>
2024-07-15 17:08:33 -0600

    estrucutar modal bottom sheet

--- modalBottomSheet/screens/prueba.js
+++ modalBottomSheet/screens/prueba.js
@@ -1,72 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Modal, Dimensions, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { FontAwesome5 } from '@expo/vector-icons';
+import {
+    BottomSheetModalProvider,
+    BottomSheetModal
+} from "@gorhom/bottom-sheet";
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 
 const { width, height } = Dimensions.get("window")
 
 export default function Prueba() {
     const [modal, setModal] = useState(false);
+
+    const sheetModal = useRef(null);
+
+    const hey = () => {
+        sheetModal?.current?.present()
+    }
+
+    const snapPoints = [width*0.3, width*0.5, width*0.9]
+
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
+        <GestureHandlerRootView style={{ flex: 1}}>
+        <BottomSheetModalProvider>
 
-        }}>
-            {/* modal */}
-            <Modal visible={modal} animationType="slide">
+            <BottomSheetModal
+            name="mon"
+            ref={sheetModal}
+            snapPoints={snapPoints}
+           >
+            <View
+            style={{
+                flex:1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Text>Contenido del BottomSheet</Text>
+                
+            </View>
+           </BottomSheetModal>
 
-                {/* vista del modal */}
-                <View
-                    style={{
-                        flex: 1,
-                        justifyContent: "center",
-                        alignItems: "center",
-                        backgroundColor: "rgba(0,0,0,0.8)",
-                    }}>
-
-                        {/* recuadro blanco modal */}
-                        <View style={{ width:width * 0.8, height:height*0.3, backgroundColor: '#fff', borderRadius:width * 0.1, padding: width*0.05 }}>
-                            {/* boton cerrar */}
-                            <TouchableOpacity onPress={() => setModal(false)} style={{ width:width * 0.2, height : width * 0.2, justifyContent:'center', alignItems:'center',backgroundColor:'#f4f4f4',borderRadius:width*0.5, left:width*0.05 }}>
-                                <FontAwesome5 name="bacon" size={width * 0.1} color="#fff" />
-
-                            </TouchableOpacity>
-
-                        </View>
-
-                </View>
-
-            </Modal >
-
-
-            {/* boton que abre el modal */}
-            <TouchableOpacity
-                onPress={() => setModal(true)}
+            <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+                <TouchableOpacity onPress={() => hey()}
                 style={{
-                    width: width * 0.8,
-                    height: height * 0.07,
-                    backgroundColor: "#0088f0",
-                    borderRadius: width * 0.1,
-                    justifyContent: "center",
-                    alignItems: "center"
+                    width: width *0.3,
+                    height: height * 0.3,
+                    backgroundColor: "#ffff",
                 }}>
+                    <Text>Hola</Text>
+                </TouchableOpacity>
+            </View>
 
-                <Text 
-                    style={{
-                        fontSize: width * 0.04,
-                        fontWeight: "700",
-                        color: "#fff"
-                    }}
-                    >
-                        ABRIR MODAL
-
-                </Text>
-
-            </TouchableOpacity>
-
-        </View >
+        </BottomSheetModalProvider>
+        </GestureHandlerRootView>
     );
 }
