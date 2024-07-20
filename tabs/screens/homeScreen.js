44fc5ae9789802ca3ab28ad6dbc2d46d643742e9
monserratvargass <guvamonse@gmail.com>
2024-07-19 20:40:15 -0600

    crear una carpeta en donde hay dos vistas

---
+++ tabs/screens/homeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text style = {{ fontSize:30, textAlign:"center", marginTop:"20%" }}>
+                Home Screen
+
+            </Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
